d93c1a2708034733d6ec5426dd07cbba59d9d2cd
Naseem <naseemsiddiqui629@gmail.com>
2025-06-23 22:46:53 +0530

    rate limit implemented

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,15 @@ import "dotenv/config"
 import { notFound } from "./middlewares/notFound.middleware.js"
 import { errorMiddleware } from "./middlewares/error.middleware.js"
 import { connectToDB } from "./config/db.config.js"
+import { mainRateLimiter } from "./middlewares/rateLimit.middleware.js"
 const app = express()
 
 app.use(urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(express.json())
+app.use(mainRateLimiter)
+const base = "/api/v1"
+app.use(`${base}/auth`)
 app.use(notFound)
 app.use(errorMiddleware)
 const PORT = process.env.PORT || 5000
---
+++ backend/middlewares/rateLimit.middleware.js
@@ -0,0 +1,16 @@
+import {rateLimit} from "express-rate-limit"
+
+
+export const mainRateLimiter = rateLimit({
+    windowMs:15*60*1000,
+    standardHeaders:true,
+    limit:150,
+    legacyHeaders:false
+})
+
+export const LoginSignupRateLimiter = rateLimit({
+    windowMs:15*60*1000,
+    standardHeaders:true,
+    limit:15,
+    legacyHeaders:false
+})
