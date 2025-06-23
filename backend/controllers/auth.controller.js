fb8763edc3802fcc1d28f7f0897386e5ffdb0500
Naseem <naseemsiddiqui629@gmail.com>
2025-06-24 00:14:20 +0530

    refresh and logout controller implemented

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,50 +1,81 @@
 import { User } from "../models/user.model.js";
 import { AsyncHandler } from "../utils/asyncHandler.js";
 import { CustomError } from "../utils/customError.js";
+import jwt from "jsonwebtoken"
 
-
-export  const signup = AsyncHandler(async(req,res,next)=>{
-    const {email,password,name} = req.body
-    if(!email || !password || !name)return next(new CustomError("All fields are required",400))
-    const existingUser = await User.findOne({email})
-    if(existingUser)return next(new CustomError("email already in use",409))
-    const user = await User.create({email,password,name})
+export const signup = AsyncHandler(async (req, res, next) => {
+    const { email, password, name } = req.body
+    if (!email || !password || !name) return next(new CustomError("All fields are required", 400))
+    const existingUser = await User.findOne({ email })
+    if (existingUser) return next(new CustomError("email already in use", 409))
+    const user = await User.create({ email, password, name })
     const userObj = user.toObject()
     userObj.password = undefined
     const accessToken = user.generateAccessToken()
     const refreshToken = user.generateRefreshToken()
-    res.cookie("refresh-token",refreshToken,{
-        maxAge:30*24*60*60*1000,
-        sameSite:"strict",
-        httpOnly:true
+    res.cookie("refresh-token", refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        sameSite: "strict",
+        httpOnly: true
     })
     return res.status(201).json({
-        success:true,
-        message:"user created",
+        success: true,
+        message: "user created",
         accessToken,
         userObj
     })
 })
-export const login = AsyncHandler(async(req,res,next)=>{
-    const {email,password} = req.body
-    if(!email || !password)return next(new CustomError("All fields are required",400))
-    const user = await User.findOne({email})
-    if(!user)return next(new CustomError("invalid credential",400))
+export const login = AsyncHandler(async (req, res, next) => {
+    const { email, password } = req.body
+    if (!email || !password) return next(new CustomError("All fields are required", 400))
+    const user = await User.findOne({ email })
+    if (!user) return next(new CustomError("invalid credential", 400))
     const isPasswordMatched = await user.comparePassword(password)
-    if(!isPasswordMatched)return next(new CustomError("invalid credential",400))
-     const accessToken = user.generateAccessToken()
+    if (!isPasswordMatched) return next(new CustomError("invalid credential", 400))
+    const accessToken = user.generateAccessToken()
     const refreshToken = user.generateRefreshToken()
     const userObj = user.toObject()
     userObj.password = undefined
-    res.cookie("refresh-token",refreshToken,{
-        maxAge:30*24*60*60*1000,
-        sameSite:"strict",
-        httpOnly:true
+    res.cookie("refresh-token", refreshToken, {
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        sameSite: "strict",
+        httpOnly: true
     })
     return res.status(200).json({
-        success:true,
-        message:"logged in successfully",
+        success: true,
+        message: "logged in successfully",
         accessToken,
         userObj
     })
+})
+
+
+export const logout = AsyncHandler((async (req, res, next) => {
+    res.cookie("refresh-token", "", {
+        maxAge: 0,
+        httpOnly: true,
+        sameSite: "strict"
+    })
+    return res.status(200).json({
+        success: true,
+        message: "logged out successfully"
+    })
+}))
+
+export const refresh = AsyncHandler(async (req, res, next) => {
+    const token = req.cookies["refresh-token"]
+    if (!token) return next(new CustomError("missing refresh token", 401))
+    try {
+        const decode = jwt.verify(token, process.env.JWT_SECRET)
+        const user = await User.findById(decode.id)
+        if(!user)return next(new CustomError("user not found",404))
+        const accessToken = user.generateAccessToken()
+        return res.status(200).json({
+            success: true,
+            message: "token refreshed",
+            accessToken
+        })
+    } catch (error) {
+        return next(new CustomError("unauthorized or malformed token",403))
+    }
 })
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,11 @@
 import express from "express"
 import { LoginSignupRateLimiter } from "../middlewares/rateLimit.middleware.js";
-import { login, signup } from "../controllers/auth.controller.js";
+import { login, logout, refresh, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router()
 
 router.route("/signup").post(LoginSignupRateLimiter,signup)
 router.route("/login").post(LoginSignupRateLimiter,login)
+router.route("/refresh").post(refresh)
+router.route("/logout").get(logout)
 export default router;
